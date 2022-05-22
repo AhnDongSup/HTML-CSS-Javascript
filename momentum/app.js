94168c98eeb4a5394a70042752c6dbc4d25b7c19
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-05-22 12:04:57 +0900

    javascript

---
+++ momentum/app.js
@@ -0,0 +1,59 @@
+// const a = 5;
+// const b = 2;
+// let myName = "dongsup";
+// console.log(a+b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log("hello " + myName);
+
+// myName = "ahndongsup";
+// console.log("hello " + myName);
+
+// const amIFat = null;
+// let something;
+// console.log(something);
+// console.log(amIFat);
+
+// const daysOfWeek = ['mon' , 1 , true , null , undefined , 'tue' , 'wed' , 'thu' , 'fri' , 'sat'];
+// console.log(daysOfWeek[5]);
+// daysOfWeek.push('sun')
+// console.log(daysOfWeek);
+
+// const player = {
+//     name: "dongsup",
+//     points: 10,
+//     fat: true,
+// };
+// console.log(player);
+// console.log(player.name);
+// console.log(player["name"]);
+// // player = false; //error
+// player.fat = false;
+// console.log(player);
+// player.lastName="Ahn";
+// console.log(player);
+// player.points+=15;
+// console.log(player.points);
+
+
+// function sayHello(nameOfperson, age){
+//     console.log("Hello my name is " + nameOfperson + " and I'm " + age);
+// }
+// function plus(a,b){
+//     console.log(a+b);
+// }
+// sayHello("dongsup",10);
+// plus(1,3);
+
+
+// const player = {
+//     name: "donsup",
+//     plus:function(a){
+//         console.log("number : " + a);
+//     },
+// };
+// player.plus(3);
+
+
+
+
