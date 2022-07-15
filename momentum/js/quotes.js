ef1e24f9eb19553559c0fe651dceb8b1a7eb8d65
tjqehd23 <96939334+tjqehd23@users.noreply.github.com>
2022-07-15 22:41:10 +0900

    javascript

---
+++ momentum/js/quotes.js
@@ -0,0 +1,16 @@
+const quotes = [
+    {
+        quoto: "아무런 소득이 없다면 객기이고 고집일뿐",
+        author: "어떤 할범",
+    },
+    {
+        quoto: "asd",
+        author: "?",
+    }
+];
+
+const quoto = document.querySelector("#queto span:first-child");
+const author = document.querySelector("#queto span:last-child");
+
+console.log(quotes[0]); 
+// 4:05
